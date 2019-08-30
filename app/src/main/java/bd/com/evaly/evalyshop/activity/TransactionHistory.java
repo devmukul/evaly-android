25cab74ee22a50dac7b6dab8e2afdbf96dd361b1
HM Tamim <hm.tamim@northsouth.edu>
2019-08-30 20:16:07 +0600

    Transaction history UI

--- app/src/main/java/bd/com/evaly/evalyshop/activity/TransactionHistory.java
+++ app/src/main/java/bd/com/evaly/evalyshop/activity/TransactionHistory.java
@@ -1,7 +1,11 @@
 package bd.com.evaly.evalyshop.activity;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import bd.com.evaly.evalyshop.R;
 
@@ -11,5 +15,27 @@ public class TransactionHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_history);
+
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setTitle("Transaction History");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0000ff")));
+
+
+
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
