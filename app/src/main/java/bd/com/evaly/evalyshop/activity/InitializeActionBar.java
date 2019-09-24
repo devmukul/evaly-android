cd66247b1f9edfe038f7b2ccff5ad4729f8b5030
HM Tamim <hm.tamim@northsouth.edu>
2019-09-24 19:43:03 +0600

    Show back button on all pages except homepage

--- app/src/main/java/bd/com/evaly/evalyshop/activity/InitializeActionBar.java
+++ app/src/main/java/bd/com/evaly/evalyshop/activity/InitializeActionBar.java
@@ -35,32 +35,31 @@ public class InitializeActionBar {
     private UserDetails userDetails;
     private Context context;
 
-
     public InitializeActionBar(LinearLayout root, MainActivity mainActivity, String type) {
 
         userDetails = mainActivity.getUserDetails();
         context = mainActivity;
-
         root.bringToFront();
         ImageView menuBtn = root.findViewById(R.id.menuBtn);
+
+        if (type.equals("home"))
+            menuBtn.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_menu));
+        else
+            menuBtn.setImageDrawable(context.getResources().getDrawable(R.drawable.ic_arrow_back));
+
         RelativeLayout notification = root.findViewById(R.id.notification_holder);
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (type.equals("home")) {
+                if (type.equals("home"))
                     mainActivity.drawer.openDrawer(Gravity.START);
-                } else {
+                else
                     mainActivity.onBackPressed();
-                }
-
-
             }
         });
 
-
         notification.setOnClickListener(v -> {
-
             if (userDetails.getToken().equals("")) {
                 mainActivity.startActivity(new Intent(mainActivity, SignInActivity.class));
             } else {
@@ -68,28 +67,15 @@ public class InitializeActionBar {
             }
         });
 
-
         ui_hot = (TextView) root.findViewById(R.id.hotlist_hot);
-
-
-
 
         if (!userDetails.getToken().equals(""))
             getNotificationCount();
 
-
-
-
-
-
-
     }
 
 
-
-
     public void getNotificationCount(){
-
 
         String url = UrlUtils.BASE_URL+"notifications_count/";
         JSONObject parameters = new JSONObject();
@@ -102,14 +88,9 @@ public class InitializeActionBar {
             @Override
             public void onResponse(JSONObject response) {
                 Log.d("onResponse", response.toString());
-
                 try {
-
                     int count = response.getInt("unread_notification_count");
-
-
                     updateHotCount(count);
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -141,7 +122,6 @@ public class InitializeActionBar {
         RequestQueue queue = Volley.newRequestQueue(context);
         queue.add(request);
 
-
     }
 
     public void updateHotCount(final int new_hot_number) {
@@ -156,7 +136,5 @@ public class InitializeActionBar {
         }
 
     }
-
-
 
 }
