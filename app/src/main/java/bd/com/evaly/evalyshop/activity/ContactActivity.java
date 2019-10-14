7c7947813742e20ac53ffaffeef8acefd4ccbf17
HM Tamim <hm.tamim@northsouth.edu>
2019-10-15 02:58:08 +0600

    New Contact Us Page UI

--- app/src/main/java/bd/com/evaly/evalyshop/activity/ContactActivity.java
+++ app/src/main/java/bd/com/evaly/evalyshop/activity/ContactActivity.java
@@ -1,6 +1,7 @@
 package bd.com.evaly.evalyshop.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,10 +9,13 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import bd.com.evaly.evalyshop.BaseActivity;
 import bd.com.evaly.evalyshop.R;
+
+import static bd.com.evaly.evalyshop.activity.ViewProductActivity.setWindowFlag;
 
 public class ContactActivity extends BaseActivity
 {
@@ -23,21 +27,25 @@ public class ContactActivity extends BaseActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
-        getSupportActionBar().setElevation(0);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Contact us");
+        //getSupportActionBar().setElevation(0);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setTitle("Contact us");
 
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        //make fully Android Transparent Status bar
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                onBackPressed();
+//                return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
