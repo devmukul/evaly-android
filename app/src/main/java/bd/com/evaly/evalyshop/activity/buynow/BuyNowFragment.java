db5f50ff59160afef4e8d21511c8760efe735208
HM Tamim <hm.tamim@northsouth.edu>
2019-10-17 00:08:38 +0600

    Rounded modal UI

--- app/src/main/java/bd/com/evaly/evalyshop/activity/buynow/BuyNowFragment.java
+++ app/src/main/java/bd/com/evaly/evalyshop/activity/buynow/BuyNowFragment.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,9 +15,19 @@ import bd.com.evaly.evalyshop.R;
 public class BuyNowFragment extends BottomSheetDialogFragment {
 
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //bottom sheet round corners can be obtained but the while background appears to remove that we need to add this.
+        setStyle(STYLE_NORMAL, R.style.BottomSheetDialogTheme);
+    }
+
+
     @Nullable @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_buy_now, container, false);
     }
 
