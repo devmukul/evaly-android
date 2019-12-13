31e7b59b72308514e4cf3297c984ea8e5d4f6aaf
HM Tamim <hm.tamim@northsouth.edu>
2019-12-14 02:53:55 +0600

    Followed shop error fixed

--- app/src/main/java/bd/com/evaly/evalyshop/activity/CampaignShopActivity.java
+++ app/src/main/java/bd/com/evaly/evalyshop/activity/CampaignShopActivity.java
@@ -8,28 +8,46 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.google.gson.JsonObject;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import bd.com.evaly.evalyshop.R;
 import bd.com.evaly.evalyshop.adapter.TabsAdapter;
+import bd.com.evaly.evalyshop.listener.DataFetchingListener;
 import bd.com.evaly.evalyshop.listener.ResponseListener;
 import bd.com.evaly.evalyshop.models.CommonSuccessResponse;
 import bd.com.evaly.evalyshop.models.TabsItem;
 import bd.com.evaly.evalyshop.models.campaign.CampaignShopItem;
+import bd.com.evaly.evalyshop.rest.apiHelper.AuthApiHelper;
 import bd.com.evaly.evalyshop.rest.apiHelper.CampaignApiHelper;
+import bd.com.evaly.evalyshop.util.UrlUtils;
 import bd.com.evaly.evalyshop.util.UserDetails;
 import bd.com.evaly.evalyshop.util.ViewDialog;
 import bd.com.evaly.evalyshop.views.StickyScrollView;
+import retrofit2.Response;
 
 public class CampaignShopActivity extends AppCompatActivity {
 
@@ -46,7 +64,7 @@ public class CampaignShopActivity extends AppCompatActivity {
     String title = "19.19 Shops";
     String slug = "evaly1919";
 
-    LinearLayout not;
+    LinearLayout not, layoutImageHolder;
     UserDetails userDetails;
     ImageView cover;
     
@@ -78,13 +96,18 @@ public class CampaignShopActivity extends AppCompatActivity {
 
         adapter = new TabsAdapter(CampaignShopActivity.this, this, itemList, 3);
         nestedSV = findViewById(R.id.sticky);
+        layoutImageHolder = findViewById(R.id.layoutImageHolder);
         recyclerView=findViewById(R.id.recycle);
         StaggeredGridLayoutManager mLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(adapter);
         dialog=new ViewDialog(CampaignShopActivity.this);
        // dialog.showDialog();
-        getEvalyShops(page);
+
+        if (slug.equals("shop-subscriptions"))
+            getFollowedShops(page);
+        else
+            getEvalyShops(page);
 
 
         progressBar.setVisibility(View.VISIBLE);
@@ -162,6 +185,93 @@ public class CampaignShopActivity extends AppCompatActivity {
 
     }
 
+
+
+    public void getFollowedShops(int p){
+
+        layoutImageHolder.setVisibility(View.GONE);
+
+        isLoading = true;
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        String url = UrlUtils.BASE_URL+"shop-subscriptions";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url ,(String) null,
+                response -> {
+                    try {
+                        isLoading = false;
+                        JSONArray jsonArray;
+
+                        if (slug.equals("shop-subscriptions"))
+                            jsonArray = response.getJSONArray("data");
+                        else
+                            jsonArray = response.getJSONArray("shops");
+
+                        boolean b=false;
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject ob = jsonArray.getJSONObject(i);
+                            TabsItem tabsItem = new TabsItem();
+                            tabsItem.setTitle(ob.getString("name"));
+                            tabsItem.setImage(ob.getString("logo_image"));
+                            tabsItem.setSlug(ob.getString("slug"));
+                            tabsItem.setCategory("root");
+                            itemList.add(tabsItem);
+                            adapter.notifyItemInserted(itemList.size());
+                        }
+
+                        if (jsonArray.length() == 0)
+                            not.setVisibility(View.VISIBLE);
+                        else
+                            not.setVisibility(View.GONE);
+
+                        // dialog.hideDialog();
+
+                        progressBar.setVisibility(View.INVISIBLE);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } , error -> {
+
+            NetworkResponse response = error.networkResponse;
+            if (response != null && response.data != null) {
+
+                if (error.networkResponse.statusCode == 401){
+
+                    AuthApiHelper.refreshToken(CampaignShopActivity.this, new DataFetchingListener<Response<JsonObject>>() {
+                        @Override
+                        public void onDataFetched(retrofit2.Response<JsonObject> response) {
+                            getEvalyShops(p);
+                        }
+
+                        @Override
+                        public void onFailed(int status) {
+                        }
+                    });
+                    return;
+                }}
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(CampaignShopActivity.this, "All shops are loaded", Toast.LENGTH_SHORT).show();
+
+            error.printStackTrace();
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                if (!userDetails.getToken().equals(""))
+                    headers.put("Authorization", "Bearer " + userDetails.getToken());
+                return headers;
+            }
+        };
+        RequestQueue rq = Volley.newRequestQueue(this);
+        request.setShouldCache(false);
+
+        request.setRetryPolicy(new DefaultRetryPolicy(50000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        rq.add(request);
+    }
 
 
 
