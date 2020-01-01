86234683633b1d8819537bee4a213c6fc7298c4e
HM Tamim <hm.tamim@northsouth.edu>
2020-01-01 21:49:45 +0600

    Evaly wallet UI done

--- app/src/main/java/bd/com/evaly/evalyshop/ui/balance/BalanceFragment.java
+++ app/src/main/java/bd/com/evaly/evalyshop/ui/balance/BalanceFragment.java
@@ -1,16 +1,21 @@
 package bd.com.evaly.evalyshop.ui.balance;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import bd.com.evaly.evalyshop.R;
 import bd.com.evaly.evalyshop.databinding.BalanceFragmentBinding;
 
 public class BalanceFragment extends BottomSheetDialogFragment {
@@ -20,6 +25,12 @@ public class BalanceFragment extends BottomSheetDialogFragment {
 
     public static BalanceFragment newInstance() {
         return new BalanceFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NORMAL, R.style.BottomSheetDialogTheme);
     }
 
     @Override
@@ -34,8 +45,15 @@ public class BalanceFragment extends BottomSheetDialogFragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(BalanceViewModel.class);
 
+        binding.balanceHolder.setVisibility(View.GONE);
+        binding.balanceHolder2.setVisibility(View.GONE);
+        binding.progressBar.setVisibility(View.VISIBLE);
 
         mViewModel.getData().observe(this, balanceModel -> {
+
+            binding.balanceHolder.setVisibility(View.VISIBLE);
+            binding.balanceHolder2.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.GONE);
 
             binding.tvBalance.setText(String.format("৳ %s", balanceModel.getBalance()));
             binding.tvHoldingBalance.setText(String.format("৳ %s", balanceModel.getHolding_balance()));
@@ -46,7 +64,20 @@ public class BalanceFragment extends BottomSheetDialogFragment {
 
         mViewModel.updateBalance();
 
+    }
 
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        BottomSheetDialog bottomSheetDialog=(BottomSheetDialog)super.onCreateDialog(savedInstanceState);
+        bottomSheetDialog.setOnShowListener(dialog -> {
+            BottomSheetDialog dialogz = (BottomSheetDialog) dialog;
+            FrameLayout bottomSheet =  dialogz.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+            BottomSheetBehavior.from(bottomSheet).setState(BottomSheetBehavior.STATE_EXPANDED);
+            BottomSheetBehavior.from(bottomSheet).setSkipCollapsed(true);
+            BottomSheetBehavior.from(bottomSheet).setHideable(true);
+        });
+        return bottomSheetDialog;
     }
 
 }
