a4dca4009ad0cc26970d32537aefe7e9a6d5f3bf
HM Tamim <hm.tamim@northsouth.edu>
2020-02-17 01:42:18 +0600

    Newsfeed crashes fixed

--- app/src/main/java/bd/com/evaly/evalyshop/ui/newsfeed/NewsfeedFragment.java
+++ app/src/main/java/bd/com/evaly/evalyshop/ui/newsfeed/NewsfeedFragment.java
@@ -904,22 +904,26 @@ public class NewsfeedFragment extends Fragment implements SwipeRefreshLayout.OnR
             public void onDataFetched(JsonObject response, int statusCode) {
                 skeletonCommentHeader.hide();
 
-                if (response == null) {
+                if (response != null && !response.get("data").isJsonNull()) {
+                    JsonObject ob = response.get("data").getAsJsonObject();
+                    JsonObject author = ob.getAsJsonObject("author");
+                    String authorName = author.get("full_name").getAsString();
+                    String authorImage = author.get("compressed_image").getAsString();
+                    boolean isAdmin = author.get("is_admin").getAsBoolean();
+                    String postText = ob.get("body").getAsString();
+                    String date = ob.get("created_at").getAsString();
+                    String postImageUrl = ob.get("attachment").getAsString();
+
+                    initCommentHeader(authorName, authorImage, isAdmin, postText, date, postImageUrl);
+
+                } else {
                     Toast.makeText(context, "Post is not available", Toast.LENGTH_SHORT).show();
                     if (commentDialog.isShowing())
                         commentDialog.dismiss();
                     return;
                 }
-                JsonObject ob = response.get("data").getAsJsonObject();
-                JsonObject author = ob.getAsJsonObject("author");
-                String authorName = author.get("full_name").getAsString();
-                String authorImage = author.get("compressed_image").getAsString();
-                boolean isAdmin = author.get("is_admin").getAsBoolean();
-                String postText = ob.get("body").getAsString();
-                String date = ob.get("created_at").getAsString();
-                String postImageUrl = ob.get("attachment").getAsString();
 
-                initCommentHeader(authorName, authorImage, isAdmin, postText, date, postImageUrl);
+
             }
 
             @Override
