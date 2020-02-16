e235a40569e4ab8fea6e21d2ff3f6c9ebc2c952e
HM Tamim <hm.tamim@northsouth.edu>
2020-02-16 23:50:54 +0600

    Campaign new UI done

--- app/src/main/java/bd/com/evaly/evalyshop/models/campaign/CampaignItem.java
+++ app/src/main/java/bd/com/evaly/evalyshop/models/campaign/CampaignItem.java
@@ -2,96 +2,132 @@ package bd.com.evaly.evalyshop.models.campaign;
 
 import com.google.gson.annotations.SerializedName;
 
+public class CampaignItem {
 
-public class CampaignItem{
+    @SerializedName("end_date")
+    private String endDate;
 
-	@SerializedName("end_date")
-	private String endDate;
+    @SerializedName("image")
+    private String image;
 
-	@SerializedName("name")
-	private String name;
+    @SerializedName("cashback_percentage")
+    private String cashbackPercentage;
 
-	@SerializedName("modified_by")
-	private String modifiedBy;
+    @SerializedName("name")
+    private String name;
 
-	@SerializedName("modified_at")
-	private String modifiedAt;
+    @SerializedName("cashback_date")
+    private String cashbackDate;
 
-	@SerializedName("slug")
-	private String slug;
+    @SerializedName("modified_by")
+    private String modifiedBy;
 
-	@SerializedName("start_date")
-	private String startDate;
+    @SerializedName("banner_image")
+    private String bannerImage;
 
-	@SerializedName("image")
-	private String image;
+    @SerializedName("modified_at")
+    private String modifiedAt;
 
-	public void setEndDate(String endDate){
-		this.endDate = endDate;
-	}
+    @SerializedName("slug")
+    private String slug;
 
-	public String getEndDate(){
-		return endDate;
-	}
+    @SerializedName("start_date")
+    private String startDate;
 
-	public void setName(String name){
-		this.name = name;
-	}
+    public String getEndDate() {
+        return endDate;
+    }
 
-	public String getName(){
-		return name;
-	}
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 
-	public void setModifiedBy(String modifiedBy){
-		this.modifiedBy = modifiedBy;
-	}
+    public String getImage() {
+        return image;
+    }
 
-	public String getModifiedBy(){
-		return modifiedBy;
-	}
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-	public void setModifiedAt(String modifiedAt){
-		this.modifiedAt = modifiedAt;
-	}
+    public String getCashbackPercentage() {
+        return cashbackPercentage;
+    }
 
-	public String getModifiedAt(){
-		return modifiedAt;
-	}
+    public void setCashbackPercentage(String cashbackPercentage) {
+        this.cashbackPercentage = cashbackPercentage;
+    }
 
-	public void setSlug(String slug){
-		this.slug = slug;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getSlug(){
-		return slug;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setStartDate(String startDate){
-		this.startDate = startDate;
-	}
+    public String getCashbackDate() {
+        return cashbackDate;
+    }
 
-	public String getStartDate(){
-		return startDate;
-	}
+    public void setCashbackDate(String cashbackDate) {
+        this.cashbackDate = cashbackDate;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 
-	@Override
- 	public String toString(){
-		return 
-			"CampaignItem{" + 
-			"end_date = '" + endDate + '\'' + 
-			",name = '" + name + '\'' + 
-			",modified_by = '" + modifiedBy + '\'' + 
-			",modified_at = '" + modifiedAt + '\'' + 
-			",slug = '" + slug + '\'' + 
-			",start_date = '" + startDate + '\'' + 
-			"}";
-		}
+    public String getBannerImage() {
+        return bannerImage;
+    }
+
+    public void setBannerImage(String bannerImage) {
+        this.bannerImage = bannerImage;
+    }
+
+    public String getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(String modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "CampaignItem{" +
+                        "end_date = '" + endDate + '\'' +
+                        ",image = '" + image + '\'' +
+                        ",cashback_percentage = '" + cashbackPercentage + '\'' +
+                        ",name = '" + name + '\'' +
+                        ",cashback_date = '" + cashbackDate + '\'' +
+                        ",modified_by = '" + modifiedBy + '\'' +
+                        ",banner_image = '" + bannerImage + '\'' +
+                        ",modified_at = '" + modifiedAt + '\'' +
+                        ",slug = '" + slug + '\'' +
+                        ",start_date = '" + startDate + '\'' +
+                        "}";
+    }
 }
