a7addafa46444c253afb947ca39a6cfa4980b16c
HM Tamim <hm.tamim@northsouth.edu>
2020-03-13 05:12:33 +0600

    bottom nav blinking issue fixed

--- app/src/main/java/bd/com/evaly/evalyshop/ui/main/MainActivity.java
+++ app/src/main/java/bd/com/evaly/evalyshop/ui/main/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,8 +24,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
@@ -189,19 +186,8 @@ public class MainActivity extends BaseActivity {
                         return false;
                     } else if (item.getItemId() == R.id.homeFragment) {
 
-
-
                         navController.popBackStack(R.id.home_nav_graph, true);
-
-                        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-
-                        int backStackEntryCount = navHostFragment.getChildFragmentManager().getBackStackEntryCount();
-
-                        Log.d("hmtz", backStackEntryCount+"");
-
-
                         navController.navigate(R.id.homeFragment);
-
                         return false;
                     } else
                         return onNavDestinationSelected(item, navController);
