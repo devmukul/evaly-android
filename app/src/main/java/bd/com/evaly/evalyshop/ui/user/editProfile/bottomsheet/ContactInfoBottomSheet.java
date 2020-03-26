42f35b086eda725ee539d9b031536c5d380061d0
HM Tamim <hm.tamim@northsouth.edu>
2020-03-26 19:26:27 +0600

    Edit Contact Info Bottom Sheet

---
+++ app/src/main/java/bd/com/evaly/evalyshop/ui/user/editProfile/bottomsheet/ContactInfoBottomSheet.java
@@ -0,0 +1,94 @@
+package bd.com.evaly.evalyshop.ui.user.editProfile.bottomsheet;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.HashMap;
+
+import bd.com.evaly.evalyshop.R;
+import bd.com.evaly.evalyshop.databinding.BottomSheetEditContactInfoBinding;
+import bd.com.evaly.evalyshop.manager.CredentialManager;
+import bd.com.evaly.evalyshop.models.user.UserModel;
+import bd.com.evaly.evalyshop.ui.user.editProfile.EditProfileViewModel;
+
+public class ContactInfoBottomSheet extends BottomSheetDialogFragment {
+
+    private BottomSheetEditContactInfoBinding binding;
+    private EditProfileViewModel viewModel;
+
+    public static ContactInfoBottomSheet newInstance() {
+        ContactInfoBottomSheet instance = new ContactInfoBottomSheet();
+        return instance;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = BottomSheetEditContactInfoBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NORMAL, R.style.TransparentBottomSheetDialog);
+        if (getActivity() != null)
+            viewModel = new ViewModelProvider(getActivity()).get(EditProfileViewModel.class);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+        bottomSheetDialog.setOnShowListener(dialog -> {
+            BottomSheetDialog dialogz = (BottomSheetDialog) dialog;
+            FrameLayout bottomSheet = dialogz.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+            assert bottomSheet != null;
+            BottomSheetBehavior.from(bottomSheet).setState(BottomSheetBehavior.STATE_EXPANDED);
+            BottomSheetBehavior.from(bottomSheet).setSkipCollapsed(true);
+            BottomSheetBehavior.from(bottomSheet).setHideable(true);
+        });
+        return bottomSheetDialog;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        UserModel userModel = CredentialManager.getUserData();
+
+        binding.email.setText(userModel.getEmail());
+        binding.phone.setText(userModel.getContacts());
+
+        binding.save.setOnClickListener(v -> {
+            String email = binding.email.getText().toString().trim();
+            String contact = binding.phone.getText().toString().trim();
+
+            HashMap<String, String> body = new HashMap<>();
+            body.put("email", email);
+            body.put("contact", contact);
+            viewModel.setUserData(body);
+        });
+
+    }
+
+}
