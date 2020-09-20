c0b2b8534180056994ac610e31e5e516573253ce
HM Tamim <hm.tamim@northsouth.edu>
2020-09-20 18:58:01 +0600

    Campaign landing page, slider implemented

--- app/src/main/java/bd/com/evaly/evalyshop/ui/campaign/CampaignFragment.java
+++ app/src/main/java/bd/com/evaly/evalyshop/ui/campaign/CampaignFragment.java
@@ -1,17 +1,30 @@
 package bd.com.evaly.evalyshop.ui.campaign;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,24 +32,45 @@ import java.util.List;
 import bd.com.evaly.evalyshop.R;
 import bd.com.evaly.evalyshop.databinding.FragmentCampaignBinding;
 import bd.com.evaly.evalyshop.listener.PaginationScrollListener;
+import bd.com.evaly.evalyshop.listener.ResponseListenerAuth;
+import bd.com.evaly.evalyshop.models.CommonResultResponse;
+import bd.com.evaly.evalyshop.models.banner.BannerItem;
 import bd.com.evaly.evalyshop.models.campaign.CampaignItem;
+import bd.com.evaly.evalyshop.rest.apiHelper.GeneralApiHelper;
 import bd.com.evaly.evalyshop.ui.campaign.adapter.CampaignAdapter;
+import bd.com.evaly.evalyshop.ui.home.controller.SliderController;
 import bd.com.evaly.evalyshop.util.ToastUtils;
+import bd.com.evaly.evalyshop.util.Utils;
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class CampaignFragment extends Fragment implements CampaignNavigator {
 
+    int coverHeight;
     private FragmentCampaignBinding binding;
     private CampaignViewModel viewModel;
     private List<CampaignItem> items;
     private CampaignAdapter adapter;
     private NavController navController;
     private boolean isLoading = false;
+    private List<BannerItem> bannerList;
+    private boolean isExpanded = true;
 
     public static CampaignFragment newInstance() {
         final CampaignFragment fragment = new CampaignFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
     @Nullable
@@ -51,19 +85,116 @@ public class CampaignFragment extends Fragment implements CampaignNavigator {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatusBarColor();
         viewModel = new ViewModelProvider(this).get(CampaignViewModel.class);
         viewModel.setNavigator(this);
     }
 
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         setupToolbar();
+        initHeader(null);
+        initSlider();
         initRecycler();
         liveEventsObserver();
     }
 
+    private void initSlider() {
+        SliderController controller = new SliderController();
+        controller.setActivity((AppCompatActivity) getActivity());
+
+        binding.sliderPager.setAdapter(controller.getAdapter());
+
+        bannerList = new ArrayList<>();
+
+        new TabLayoutMediator(binding.sliderIndicator, binding.sliderPager,
+                (tab, position) -> {
+                    tab.setText("");
+
+                }
+        ).attach();
+
+        binding.sliderIndicator.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                initHeader(bannerList.get(tab.getPosition()).getImage());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        GeneralApiHelper.getBanners(new ResponseListenerAuth<CommonResultResponse<List<BannerItem>>, String>() {
+            @Override
+            public void onDataFetched(CommonResultResponse<List<BannerItem>> response, int statusCode) {
+                bannerList = response.getData();
+                controller.reAddData(response.getData());
+                controller.requestModelBuild();
+            }
+
+            @Override
+            public void onFailed(String errorBody, int errorCode) {
+
+            }
+
+            @Override
+            public void onAuthError(boolean logout) {
+
+            }
+        });
+    }
+
     private void setupToolbar() {
+
+
+        isExpanded = true;
+        Rect rectangle = new Rect();
+        Window window = getActivity().getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(rectangle);
+        int statusBarHeight = rectangle.top;
+
+        // binding.collapsingToolbar.setScrimVisibleHeightTrigger((binding.appBar.getHeight()-1));
+        coverHeight = ((binding.coverHolder.getHeight()) + statusBarHeight + (int) Utils.convertDpToPixel(60, getContext())) * -1;
+        coverHeight = (int) (Utils.convertDpToPixel(100, getContext())) * -1;
+        binding.appBar.addOnOffsetChangedListener((appBarLayout, verticalOffset) -> {
+            appBarLayout.post(() -> {
+                if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+                    if (isExpanded)
+                        return;
+                    isExpanded = true;
+                    darkStatusBar();
+                    getActivity().getWindow().setStatusBarColor(Color.WHITE);
+                    binding.backArrow.setColorFilter(Color.parseColor("#000000"), PorterDuff.Mode.SRC_ATOP);
+                    binding.searchText.setTextColor(Color.parseColor("#777777"));
+                    binding.searchContainer.setBackground(getResources().getDrawable(R.drawable.input_brd_round_light));
+                } else {
+                    if (!isExpanded)
+                        return;
+                    isExpanded = false;
+                    binding.collapsingToolbar.setScrimsShown(false, true);
+                    if (getActivity() != null && getActivity().getWindow() != null) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            getActivity().getWindow().getDecorView().setSystemUiVisibility(0);
+                            getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+                        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+                            getActivity().getWindow().setStatusBarColor(Color.WHITE);
+                    }
+
+                    binding.backArrow.setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_ATOP);
+                    binding.searchText.setTextColor(Color.parseColor("#ffffff"));
+                    binding.searchContainer.setBackground(getResources().getDrawable(R.drawable.input_brd_round_dark));
+                }
+            });
+        });
+
+
 //        binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
 //        binding.toolbar.setNavigationOnClickListener(view1 -> {
 //            if (getActivity() != null)
@@ -118,7 +249,6 @@ public class CampaignFragment extends Fragment implements CampaignNavigator {
         });
     }
 
-
     private void initRecycler() {
         items = new ArrayList<>();
         adapter = new CampaignAdapter(getContext(), items, item -> {
@@ -139,6 +269,14 @@ public class CampaignFragment extends Fragment implements CampaignNavigator {
                 }
             }
         });
+    }
+
+    private void initHeader(String url) {
+        Glide.with(binding.coverImage)
+                .asBitmap()
+                .load(url == null ? R.drawable.cover : url)
+                .apply(RequestOptions.bitmapTransform(new BlurTransformation(15, 2)))
+                .into(binding.coverImage);
     }
 
     @Override
@@ -163,6 +301,39 @@ public class CampaignFragment extends Fragment implements CampaignNavigator {
             return;
         ToastUtils.show("Error occurred!");
         //  binding.progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    private void setStatusBarColor() {
+        if (getActivity() != null && getActivity().getWindow() != null) {
+            getActivity().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            setWindowFlag(getActivity(), WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    private void darkStatusBar() {
+        if (getActivity() != null && getActivity().getWindow() != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                int flags = getActivity().getWindow().getDecorView().getSystemUiVisibility();
+                flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                getActivity().getWindow().getDecorView().setSystemUiVisibility(flags);
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getActivity().getWindow().setStatusBarColor(Color.BLACK);
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (getActivity() != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                getActivity().getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                getActivity().getWindow().setStatusBarColor(Color.WHITE);
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getActivity().getWindow().setStatusBarColor(Color.WHITE);
+            }
+        }
     }
 }
 
