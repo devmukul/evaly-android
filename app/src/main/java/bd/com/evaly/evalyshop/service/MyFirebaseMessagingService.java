709c4d3fd16d9c55a76970b9b575ed2ac63b09db
HM Tamim <hm.tamim@evaly.com.bd>
2020-12-16 01:26:49 +0600

    Load notification image with glide

--- app/src/main/java/bd/com/evaly/evalyshop/service/MyFirebaseMessagingService.java
+++ app/src/main/java/bd/com/evaly/evalyshop/service/MyFirebaseMessagingService.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -13,41 +14,44 @@ import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.FutureTarget;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.google.gson.Gson;
 import com.orhanobut.logger.Logger;
 
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 import bd.com.evaly.evalyshop.R;
 import bd.com.evaly.evalyshop.ui.issue.IssuesActivity;
 import bd.com.evaly.evalyshop.ui.main.MainActivity;
 import bd.com.evaly.evalyshop.ui.order.orderDetails.OrderDetailsActivity;
-import bd.com.evaly.evalyshop.util.firebase.generatePictureStyleNotification;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Logger.d(new Gson().toJson(remoteMessage.getData()));
+        Map<String, String> data = remoteMessage.getData();
         try {
-            if (remoteMessage.getData().size() > 0) {
-                String title = remoteMessage.getData().get("title");
-                String body = remoteMessage.getData().get("body");
-                String type = remoteMessage.getData().get("type");
-                String resource_id = remoteMessage.getData().get("resource_id");
-                if (remoteMessage.getData().containsKey("image_url") && type.equals("image") && resource_id != null && !resource_id.isEmpty()) {
-                    String imageUrl = remoteMessage.getData().get("image_url");
-                    new generatePictureStyleNotification(this, title, body, type,
-                            imageUrl, resource_id).execute();
-                } else
-                    sendNotification(body, title, resource_id, type);
+            if (data.size() > 0) {
+                String title = data.get("title");
+                String body = data.get("body");
+                String type = data.get("type");
+                String resource_id = data.get("resource_id");
+                String imageUrl = null;
+                if (data.containsKey("image_url"))
+                    imageUrl = data.get("image_url");
+                sendNotification(body, title, resource_id, type, imageUrl);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void sendNotification(String messageBody, String messageTitle, String resourceId, String type) {
+    private void sendNotification(String messageBody, String messageTitle, String resourceId, String type, String imageUrl) {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         String NOTIFICATION_CHANNEL_ID = "Evaly_NOTIFICATION";
 
@@ -76,7 +80,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 intent.putExtra("notification_type", type);
                 intent.putExtra("resource_id", resourceId);
             }
-            intent.putExtra("pageUrl", resourceId);
         }
 
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent,
@@ -87,12 +90,35 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         notificationBuilder.setSmallIcon(R.drawable.ic_status_icon)
                 .setContentTitle(messageTitle)
                 .setContentText(messageBody)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody))
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setPriority(Notification.PRIORITY_MAX)
                 .setContentIntent(pendingIntent);
 
+        if (imageUrl != null) {
+            try {
+                FutureTarget<Bitmap> bitmapFutureTarget =
+                        Glide.with(getApplicationContext())
+                                .asBitmap()
+                                .load(imageUrl)
+                                .submit();
+                Bitmap imageBitmap = bitmapFutureTarget.get();
+
+                NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle()
+                        .bigPicture(imageBitmap)
+                        .setBigContentTitle(messageTitle)
+                        .setSummaryText(messageBody);
+                notificationBuilder.setStyle(bigPictureStyle);
+                Glide.with(getApplicationContext()).clear(bitmapFutureTarget);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody));
+        }
+
+        assert notificationManager != null;
         notificationManager.notify(1, notificationBuilder.build());
     }
 }
