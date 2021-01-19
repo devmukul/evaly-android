d8ce73b873ab11cfa28c59f84bdbdc3136f1fb81
HM Tamim <hm.tamim@northsouth.edu>
2021-01-19 12:00:06 +0600

    Migrated to epoxy 4.3.1

--- app/src/main/java/bd/com/evaly/evalyshop/package-info.java
+++
@@ -1,6 +0,0 @@
-@EpoxyDataBindingLayouts({R.layout.divider_view, R.layout.express_product_title, R.layout.home_model_express_skeleton})
-@PackageModelViewConfig(rClass = R.class)
-package bd.com.evaly.evalyshop;
-
-import com.airbnb.epoxy.EpoxyDataBindingLayouts;
-import com.airbnb.epoxy.PackageModelViewConfig;
