d3f91e5afb2dc8edafa9d965e38f91aa54e52ddf
HM Tamim <hm.tamim@evaly.com.bd>
2021-02-22 22:28:05 +0600

    db version increased

--- app/src/main/java/bd/com/evaly/evalyshop/data/roomdb/AppDatabase.java
+++ app/src/main/java/bd/com/evaly/evalyshop/data/roomdb/AppDatabase.java
@@ -16,7 +16,7 @@ import bd.com.evaly.evalyshop.models.banner.BannerItem;
 import bd.com.evaly.evalyshop.models.express.ExpressServiceModel;
 import bd.com.evaly.evalyshop.models.profile.AddressResponse;
 
-@Database(entities = {CategoryEntity.class, WishListEntity.class, BannerItem.class, ExpressServiceModel.class, AddressResponse.class}, version = 24, exportSchema = false)
+@Database(entities = {CategoryEntity.class, WishListEntity.class, BannerItem.class, ExpressServiceModel.class, AddressResponse.class}, version = 26, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static AppDatabase instance;
